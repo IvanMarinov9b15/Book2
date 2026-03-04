75116b22d0122242f80a4c877f8a43e26cb733f6
IvanMarinov9b15 <ipmarinov248b@gmail.com>
2026-03-04 14:02:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 st.title("App")
 
-if "book" not in st.session_state:
-  st.session_state.book = []
+if "books" not in st.session_state:
+  st.session_state.books = []
 
 st.header("Add book")
 title = st.text_input("Title")
@@ -16,25 +16,27 @@ if st.button("Add the book"):
     "price" : price
   }
 
-st.session_state.book.append(book)
+st.session_state.books.append(book)
 st.success("The book has been added!")
 
 if st.button("Show all books"):
-  if len(st.session_state.book) == 0:
-    st.write("There are no added books.")
-  else:
-    for book in st.session_state.book:
-      st.write("Title:", book["title"])
-      st.write("Author:", book["author"])
-      st.write("Price:", book["price"])
-      st.write("--------------------------")
+  
+if len(st.session_state.book) == 0:
+  st.write("There are no added books.")
+else:
+  for book in st.session_state.books:
+    st.write("Title:", book["title"])
+    st.write("Author:", book["author"])
+    st.write("Price:", book["price"])
+    st.write("--------------------------")
 
 st.header("Searching by author")
 search_author = st.text_input("Enter name of author")
 if st.button("Search by author"):
-  found = False
+  
+found = False
 
-for book in st.session_state.book:
+for book in st.session_state.books:
   if book["author"] == search_author:
     st.write(book)
     found = True
